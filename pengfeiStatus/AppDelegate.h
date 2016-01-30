f8dc492a45491671dcf88158a0595416b655dbed
pengfei2015 <wenpengfei@wenpengfeidemacbook-pro.local>
2016-01-30 17:20:06 +0800

    Initial Commit

---
+++ pengfeiStatus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pengfeiStatus
+//
+//  Created by 温鹏飞 on 16/1/30.
+//  Copyright © 2016年 温鹏飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
